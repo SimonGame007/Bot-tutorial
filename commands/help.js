1a1df52b97fa15ae08de2bf28859eb2b6784cdde
SimonGamer007 <77907916+SimonGame007@users.noreply.github.com>
2021-05-16 10:53:07 -0300

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -3,5 +3,6 @@ const Discord = require("discord.js");
 exports.run = async (client, message, args) => {
   const embed = new Discord.MessageEmbed()
   .setDescription(`Olá ${message.author} meu prefixo é l! e aqui estão meus comandos: `)
+  .setFooter(`comando execultado por ${message.author.id}`)
   message.channel.send(embed)
 }
